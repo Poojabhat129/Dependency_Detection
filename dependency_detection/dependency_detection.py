0aa31342bd801663775bff4e85160c8dca9f2415
Santosh Thoduka <santosh.thoduka@gmail.com>
2019-07-10 15:49:04 +0200

    add plotting of pearson correlation coefficient to windowed plots; save all plots

--- dependency_detection/dependency_detection.py
+++ dependency_detection/dependency_detection.py
@@ -4,17 +4,19 @@ from statsmodels.tsa import stattools
 from statsmodels.tsa.stattools import grangercausalitytests
 
 import numpy as np
+import scipy
 
-class GrangerCausality(object):
-    def __init__(self, significance_level):
-        self.significance_level = significance_level
+class DependencyDetection(object):
+    def __init__(self):
+        pass
 
-    def is_granger_causal(self, x1, x2, lag=None):
+    @staticmethod
+    def is_granger_causal(x1, x2, lag=None, significance_level=0.05):
         '''
         does x2 granger cause x1?
         '''
         if lag is None:
-            lag = self.find_optimal_lag(x1, x2)
+            lag = int((x1.shape[0] - 1) / 3) - 1
 
         input_data = np.column_stack((x1, x2))
         results = grangercausalitytests(input_data, maxlag=lag, verbose=False)
@@ -27,9 +29,10 @@ class GrangerCausality(object):
         pvalues = pvalues[pvalues >= mean_pvalue - std_pvalue]
         pvalues = pvalues[pvalues <= mean_pvalue + std_pvalue]
         causal = False
-        if (np.min(pvalues) < self.significance_level):
+        if (np.min(pvalues) < significance_level):
             causal = True
         return causal, np.argmin(pvalues), np.min(pvalues)
 
-    def find_optimal_lag(self, x1, x2):
-        return int((x1.shape[0] - 1) / 3) - 1
+    @staticmethod
+    def get_correlation_coefficient(x1, x2):
+        return scipy.stats.pearsonr(x1, x2)[0]
